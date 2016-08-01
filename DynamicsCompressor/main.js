dedca849c5dc21eec4f3949fcbcb889447f4d2f7
sole <sole@mozilla.com>
2016-08-01 14:09:30 +0100

    Make DynamicsCompressor example more solid by detecting support for AudioParam or not

--- DynamicsCompressor/main.js
+++ DynamicsCompressor/main.js
@@ -8,12 +8,31 @@ window.onload = function() {
 	mediaElementSource.connect(compressor);
 
 	var reduction = document.getElementById('reduction');
+	var getReductionValue = makeReductionFunction();
 
 	requestAnimationFrame(render);
 
 	function render(t) {
 		requestAnimationFrame(render);
+		reduction.value = getReductionValue(compressor);
+	}
 
-		reduction.value = compressor.reduction;
+	// A recent Web Audio API has changed the type of reduction from AudioParam to float
+	// This function generates a function that can be used to obtain the reduction value
+	// from any compressor
+	function makeReductionFunction() {
+		// Create temporary audio context and compressor to check for properties
+		var ac = new AudioContext();
+		var co = ac.createDynamicsCompressor();
+		if(co.reduction.value !== undefined) {
+			console.log('old school');
+			return function(compressor) {
+				return compressor.reduction.value;
+			}
+		} else {
+			return function(compressor) {
+				return compressor.reduction;
+			}
+		}
 	}
 };
